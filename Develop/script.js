890d82d49ae14cc8b610a8c4c4abd244234ee5de
lexslie <lesliiee727@gmail.com>
2023-05-17 14:04:20 -0700

    included specific characters eligible for password criteria, and the prompt for password length

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,22 @@
 // Assignment Code
+
+// Password criteria
+var lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var numeric = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var specialCharacters = ['!', '"', '#', '$', '%', '&', '*', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '^', '_', '~'];
+
+var generatePassword = function() {
+  // Prompting user for desired password length
+  var passwordLength = window.prompt("Enter a desired password length between 8 and 128 characters:");
+
+  // If user pressed Cancel, immediately end function
+  if (!userChoice) {
+    return;
+  }
+
+}
+
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
